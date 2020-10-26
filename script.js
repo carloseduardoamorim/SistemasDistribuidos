69cb2dc9fc4da7e625018797e089c6a53a2aa974
Carlos Eduardo Amorim <34717046+carloseduardoamorim@users.noreply.github.com>
2020-10-25 22:39:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-onload = request();
-
 async function request(){
     const response = await fetch('https://jsonplaceholder.typicode.com/photos');
     const object = await response.json();
 
-    const output = "<h2>Dados obtidos do servidor!</h2>" + JSON.stringify(object);
+    const outputJson = JSON.stringify(object);
+    const outputConfirm = "<h2>Dados obtidos do servidor!</h2>";
 
-    document.getElementById("dados").innerHTML = output;
+    document.getElementById("dados").innerHTML = outputJson;
+    document.getElementById("confirm").innerHTML = outputConfirm;
 }
+
+request();
